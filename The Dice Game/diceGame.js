c4bc8485f9d2df3a7a586038b3382f8c99f213d0
usama-muf <usama.muf@gmail.com>
2020-05-31 16:31:47 +0530

    The Dice GAme Completed

--- The Dice Game/diceGame.js
+++ The Dice Game/diceGame.js
@@ -6,3 +6,162 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 */
+
+var scores, roundScore,activePlayer,gamePlay;
+
+init();
+//dice=Math.floor(Math.random()*6)+1;
+
+//      DOM manipulation
+
+//document.querySelector('#current-'+activePlayer).textContent=dice;//selects value
+
+//document.querySelector('#current-'+ activePlayer).innerHTML='<em>'+dice+'</em>'
+
+//var x = document.querySelector('#score-0').textContent;
+
+//console.log(x);
+
+document.querySelector('.dice').style.display='none';
+
+
+
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    if(gamePlay)
+    
+    {
+        //Random No
+    var dice=Math.floor(Math.random()*6)+1;
+    
+    // 2 display the no
+   var diceDOM= document.querySelector('.dice');
+   diceDOM.style.display='block';
+    diceDOM.src='dice-'+dice+'.png';
+    
+    //3 update the roundScore if currentNo is not 1
+        if(dice!==1){
+            roundScore=roundScore+dice;
+            document.querySelector('#current-'+ activePlayer).textContent=roundScore;
+        }
+        
+        else{
+            nextPlayer();
+        }
+    }
+});
+
+
+// to add roundscore in main score
+
+document.querySelector('.btn-hold').addEventListener('click',function(){
+    
+   /* if(activePlayer===0)
+    {
+        scores[0]+=roundScore;
+    
+        document.querySelector('#score-'+ activePlayer).textContent= scores[0];
+    }
+    else
+    {
+        scores[1]+=roundScore;
+    
+        document.querySelector('#score-'+ activePlayer).textContent= scores[1];
+    }
+     */
+    if(gamePlay)
+        {
+    scores[activePlayer]+=roundScore;
+   
+    document.querySelector('#score-'+ activePlayer).textContent=scores[activePlayer];
+    
+    
+   
+    if(scores[activePlayer]>=100){
+        document.querySelector('#name-'+activePlayer).textContent='WINNER!';
+        
+        document.querySelector('.dice').style.display='none';
+
+        document.querySelector('.player-' +activePlayer+'-panel').classList.add('winner');
+        
+        document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+        gamePlay=false;
+        
+    }
+    
+else    
+    nextPlayer();
+        }
+});
+
+document.querySelector('.btn-new').addEventListener('click',function(){
+   
+    init();
+    
+    
+    //document.querySelector('player-'+activePlayer+'-panel').classList.remove('winner');
+    //document.querySelector('.palyer-0-panel').classList.add('active');
+    
+});
+
+
+
+function nextPlayer(){
+    activePlayer===0? activePlayer=1:activePlayer=0;         roundScore=0;
+    
+    
+            
+            document.getElementById('current-0').textContent='0';
+            document.getElementById('current-1').textContent='0';
+    
+    
+     //active class: only active in palyer-0-panel class then we can change class when necessary or giving property to another class
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    
+    
+}
+
+
+function init(){
+    scores=[0,0];
+roundScore=0;
+activePlayer=0;
+    gamePlay=true;
+    document.getElementById('score-0').textContent='0';
+    document.getElementById('score-1').textContent='0';
+    document.getElementById('current-0').textContent='0';
+    document.getElementById('current-1').textContent='0';
+    //var name-0=prompt("Enter Name of 1st Player: ");
+    //var name-2=prompt("Enter Name of 2nd Player: ");
+    document.getElementById('name-0').textContent='Player 1';
+    document.getElementById('name-1').textContent='Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');   
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
