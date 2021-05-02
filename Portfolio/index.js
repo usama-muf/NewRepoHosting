07f6a242ff0b4167a94e6ba1ec168b5b66857e4f
usama-muf <usama.muf@gmail.com>
2021-05-02 13:19:08 +0530

    update

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -1,4 +1,4 @@
-function checkForm(form) {
+/*function checkForm(form) {
     val1=document.getElementById('entry.451699722').value;
     if (val1=="" ) {
         alert("Error: You Entered Null value in Name section ❌");
@@ -22,17 +22,41 @@ function checkForm(form) {
     }
 
 }
+*/
+///////////////////////////
+//section Animation
+const section = document.querySelectorAll(".section");
 
+const revealSection = function (entrys, observer) {
+  const [entry] = entrys;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  console.log(entry);
+  observer.unobserve(entry.target);
+};
 
-var d= new Date();
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0,
+});
+
+section.forEach((sec) => {
+  sectionObserver.observe(sec);
+  sec.classList.add("section--hidden");
+});
+
+/////////////////////////
+
+////////////////////////
+var d = new Date();
 const n = d.getFullYear();
 function current() {
-    return n;
+  return n;
 }
 
 var tag = document.createElement("p");
-var newtext= ' copyright © '+current();
-   var text = document.createTextNode(newtext);
-   tag.appendChild(text);
-   var element = document.getElementById("new");
-   element.appendChild(tag);
+var newtext = " copyright © " + current();
+var text = document.createTextNode(newtext);
+tag.appendChild(text);
+var element = document.getElementById("new");
+element.appendChild(tag);
