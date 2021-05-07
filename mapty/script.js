0701c04c49183bd9f30cfa572f4e230b85fab783
usama-muf <usama.muf@gmail.com>
2021-05-07 16:53:49 +0530

    new app

--- mapty/script.js
+++ mapty/script.js
@@ -11,42 +11,99 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-var options = {
-  enableHighAccuracy: true,
-  maximumAge: 10000,
-  timeout: 5000,
-  zoom: 30,
-};
+class App {
+  #map;
+  #mapEvent;
 
-if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
-      const coords = [latitude, longitude];
-      console.log(
-        `https://www.google.com/maps/@${latitude},${longitude},18.35z/data=!5m1!1e4`
+  constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._adjustRunningCycling);
+  }
+
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Location cannot be fetched');
+        }
       );
-      const map = L.map('map', {
-        maxZoom: 18,
-        minZoom: 13,
-      }).setView(coords, 13);
+  }
 
-      map.setZoom(17);
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+  _loadMap(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coords = [latitude, longitude];
+    console.log(
+      `https://www.google.com/maps/@${latitude},${longitude},18.35z/data=!5m1!1e4`
+    );
 
-      const circle = L.circle(coords, {
-        color: '#2b282883',
-        fillColor: '##d16882',
-        fillOpacity: 0.5,
-        radius: 50,
-      }).addTo(map);
-    },
-    function () {
-      alert('Location cannot be fetched');
-    },
-    options
-  );
+    this.#map = L.map('map', {
+      maxZoom: 18,
+      minZoom: 13,
+    }).setView(coords, 13);
+
+    this.#map.setZoom(17);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    this.#map.on('click', this._showForm.bind(this));
+
+    ////Deals with clicks on map//
+    const circle = L.circle(coords, {
+      color: '#2b282883',
+      fillColor: '##d16882',
+      fillOpacity: 0.5,
+      radius: 50,
+    }).addTo(this.#map);
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _adjustRunningCycling() {
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(event) {
+    event.preventDefault();
+
+    //removing values from form
+    inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value =
+      '';
+    inputDistance.focus();
+
+    //display marker
+    //console.log(mapEvent);
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('<p>You just clicked here</p>')
+      .openPopup();
+
+    form.classList.add('hidden');
+  }
+}
+//
+//
+//
+//
+
+const app = new App();
