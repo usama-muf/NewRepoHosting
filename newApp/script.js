db90e120bacf59a98b80ad289263f91f02fedc78
usama-muf <usama.muf@gmail.com>
2021-06-23 11:59:41 +0530

    New app styles update

--- newApp/script.js
+++ newApp/script.js
@@ -52,3 +52,29 @@ buttonRegister.addEventListener("click", function (event) {
   openPage.classList.add("hidden");
   mainContent.classList.remove("hidden");
 });
+
+//Leaflet library starts
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    const coords = [latitude, longitude];
+
+    var map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+      .openPopup();
+  },
+  function () {
+    alert("cannot fetch location");
+  }
+);
